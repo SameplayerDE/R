d0ab73b2f3098820416db8fcb6180c0268ee4e51
SameplayerDE <asameplayer@gmail.com>
2021-12-24 12:53:04 +0100

    changes

--- script_0.R
+++ script_0.R
@@ -1,4 +1,12 @@
-data <- read.csv('messung.csv') #load data from csv
-dataPlayers <- read.csv('spieler.csv') #load data from csv
+load <- function(path) {
+	if (is.character(path)) {
+		return (read.csv(path))
+	}
+	stop("Path has to be a string")
+}
+
+data <- load("messung.csv") #load data from csv
+dataPlayers <- load("spieler.csv") #load data from csv
 retval <- subset(data, SpielerID == 1)
+print("Done")
 write.csv(retval, "o.csv")
